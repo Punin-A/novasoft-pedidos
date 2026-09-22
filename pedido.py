4a13ee5e2ce8d7e73c370688a4362a2f74b98dc8
Punin-A <angelpuninaarellano@gmail.com>
2026-09-22 15:55:06 -0500

    Revert "Cambiar calculo de total de pedidos"
    This reverts commit 656e637c5238ee3733a79c7f35a7e2eb688dd6ab.

--- pedido.py
+++ pedido.py
@@ -1,4 +1,4 @@
 def calcular_total(precio, cantidad):
     if precio < 0 or cantidad < 0:
         raise ValueError("El precio y la cantidad no pueden ser negativos")
-    return round(precio + cantidad, 2)
+    return round(precio * cantidad, 2)
